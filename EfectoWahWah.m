c9672aa98bd731ce8f5acf880ceddf21a3d2750c
grilluz <andrea.grillo96@gmail.com>
2017-11-09 14:14:03 +0100

    delete all variables and keep only yb
    it calculates yl with the prebuilt function sum(vector)

--- EfectoWahWah.m
+++ EfectoWahWah.m
@@ -3,20 +3,17 @@ Q1 = 2*damp;
 Fc = minf;
 F1 = 2*sin((pi*Fc)/Fs);
 y(1) = F1*x(1);
-yl = F1*y(1);
-yaux = y(1);
 delta = Fw/Fs;
+
 for n=2:length(x),
     if Fc + delta > maxf | Fc + delta < minf 
         delta =  -delta;
     end
     Fc = Fc+ delta;
     F1 = 2*sin((pi*Fc)/Fs);
-    yh = x(n)-yl-Q1*yaux;
-    y(n) = F1*yh+yaux;
-    yl=F1*y(n)+yl;
-    yaux=y(n);
+    y(n) = F1*x(n)-F1*Q1*y(n-1)+y(n-1)-F1*F1*sum(y(1:n-1));
 end
+%normalise
 maxy = max(abs(y));
 y = y/maxy;
 
