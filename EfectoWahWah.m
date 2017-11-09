472eb4f24bbfde6e1a2adeb8ef30ad7096e3b20c
grilluz <andrea.grillo96@gmail.com>
2017-11-09 14:16:08 +0100

    sum() is too heavy!
    the function sum is too heavy for the program, so it's better use an
    auxiliare variable

--- EfectoWahWah.m
+++ EfectoWahWah.m
@@ -3,6 +3,7 @@ Q1 = 2*damp;
 Fc = minf;
 F1 = 2*sin((pi*Fc)/Fs);
 y(1) = F1*x(1);
+ysum = 0;
 delta = Fw/Fs;
 
 for n=2:length(x),
@@ -11,7 +12,8 @@ for n=2:length(x),
     end
     Fc = Fc+ delta;
     F1 = 2*sin((pi*Fc)/Fs);
-    y(n) = F1*x(n)-F1*Q1*y(n-1)+y(n-1)-F1*F1*sum(y(1:n-1));
+    y(n) = F1*x(n)+(1-F1*Q1)*y(n-1)-F1*F1*ysum;
+	ysum = ysum + y(n);
 end
 %normalise
 maxy = max(abs(y));
